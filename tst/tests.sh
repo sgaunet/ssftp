3558c8ce6091390e77daec5171e3530e034c1d56
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2021-07-25 21:32:52 +0200

    Improve stdout informations

--- tst/tests.sh
+++ tst/tests.sh
@@ -9,11 +9,11 @@ projectWorkdir=$(dirname $tstDir)
 
 vagrant destroy -f
 vagrant up
-echo "Create /tmp/toto (100 MB with random data)"
+echo -e "\nCreate /tmp/toto (100 MB with random data)"
 dd if=/dev/urandom of=/tmp/toto  bs=1024 count=102400
 
 
-echo "# Test : upload a file"
+echo -e "\n\n# Test : upload a file"
 cd "$projectWorkdir/src"
 go run . -i ${projectWorkdir}/tst/.vagrant/machines/ex-0/virtualbox/private_key /tmp/toto vagrant@10.0.50.10:/tmp/toto
 rc=$?
@@ -24,7 +24,7 @@ then
     exit 1
 fi
 
-echo "# Test 2: upload a file"
+echo -e "\n\n# Test 2: upload a file to a dir of the same name"
 ssh -i ${projectWorkdir}/tst/.vagrant/machines/ex-0/virtualbox/private_key \
      vagrant@10.0.50.10 'rm /tmp/toto' 
 echo $?
@@ -42,6 +42,7 @@ then
     exit 1
 fi
 
+echo -e "\n\n# Test 3 : Download the file /tmp/toto/toto to /tmp/toto"
 go run . -i ${projectWorkdir}/tst/.vagrant/machines/ex-0/virtualbox/private_key  vagrant@10.0.50.10:/tmp/toto/toto /tmp/toto
 rc=$?
 
@@ -51,6 +52,7 @@ then
     exit 1
 fi
 
+echo -e "\n\n# Test 4 : Same test"
 go run . -i ${projectWorkdir}/tst/.vagrant/machines/ex-0/virtualbox/private_key  vagrant@10.0.50.10:/tmp/toto/toto /tmp/toto
 rc=$?
 
@@ -61,7 +63,7 @@ then
 fi
 
 
-echo "# Test recursive upload"
+echo -e "\n\n# Test 5 : recursive upload ../src to /tmp"
 go run . -i ${projectWorkdir}/tst/.vagrant/machines/ex-0/virtualbox/private_key  ../src vagrant@10.0.50.10:/tmp
 rc=$?
 
@@ -70,3 +72,16 @@ then
     echo "Error when uploading a file"
     exit 1
 fi
+
+echo -e "\n\n# Test 6 : recursive download /tmp/src to /tmp"
+go run . -i ${projectWorkdir}/tst/.vagrant/machines/ex-0/virtualbox/private_key   vagrant@10.0.50.10:/tmp/src /tmp/
+rc=$?
+
+if [ "$rc" != "0" ]
+then
+    echo "Error when uploading a file"
+    exit 1
+fi
+
+rm -rf /tmp/src
+
