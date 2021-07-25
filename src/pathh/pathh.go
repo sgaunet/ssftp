26177893d28e1ca0172d537538801219b239f0ff
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2021-07-25 21:03:51 +0200

    Add comments

--- src/pathh/pathh.go
+++ src/pathh/pathh.go
@@ -13,6 +13,7 @@ type Path interface {
 	GetFilePath() string
 }
 
+// struct to handle path manipulation (local or remote : user@server:dir)
 type path struct {
 	path string
 }
@@ -24,6 +25,7 @@ func New(srcPath string) Path {
 	return p
 }
 
+// Return if the path seems to be a remote path
 func (p path) IsRemote() bool {
 	// r, _ := regexp.Compile("([a-z]+)@([a-z]+)")
 	// r, _ := regexp.Compile("([a-z]+)@([a-z]+)")
@@ -37,10 +39,12 @@ func (p path) IsRemote() bool {
 	return match
 }
 
+// Return if the path seems to be a local path
 func (p path) IsLocal() bool {
 	return !p.IsRemote()
 }
 
+// If remote, will return the user
 func (p path) GetUser() string {
 	splitted := strings.Split(p.path, "@")
 	if splitted[0] == p.path {
@@ -49,6 +53,7 @@ func (p path) GetUser() string {
 	return splitted[0]
 }
 
+// If remote, will return the server (IP or dns name)
 func (p path) GetServer() string {
 	splitted := strings.Split(p.path, "@")
 	if splitted[0] == p.path {
@@ -59,6 +64,7 @@ func (p path) GetServer() string {
 	return splitted[0]
 }
 
+// Return the path (for a local or remote path)
 func (p path) GetFilePath() string {
 
 	if p.IsRemote() {
