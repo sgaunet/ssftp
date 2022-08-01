67dfcb7787d0118aab5f44612a86d3e4812bdbb2
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2022-08-01 23:47:02 +0200

    fix regexp #3

--- src/pathh/pathh.go
+++ src/pathh/pathh.go
@@ -28,16 +28,8 @@ func New(srcPath string) Path {
 
 // Return if the path seems to be a remote path
 func (p path) IsRemote() bool {
-	// r, _ := regexp.Compile("([a-z]+)@([a-z]+)")
-	// r, _ := regexp.Compile("([a-z]+)@([a-z]+)")
-	// return r.Match([]byte(p.path))
-	ipv6_regex := `^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$`
-	ipv4_regex := `^(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4})`
-	domain_regex := `^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$`
-	localserver_regex := `^[a-zA-Z0-9]+$`
-
-	match, _ := regexp.MatchString(ipv4_regex+`|`+ipv6_regex+`|`+domain_regex+`|`+localserver_regex, p.GetServer())
-	return match
+	r, _ := regexp.Compile(".*@.*:.+")
+	return r.Match([]byte(p.path))
 }
 
 // Return if the path seems to be a local path
